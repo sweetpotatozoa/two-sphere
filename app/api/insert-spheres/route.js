dd59e2c223b62a84ed2656b5baa318e38f92b75e
sweetpotatozoa <tkddn3200@korea.ac.kr>
2024-11-15 00:03:07 +0900

    fix : 백오피스 대용 수정

--- app/api/insert-spheres/route.js
+++ app/api/insert-spheres/route.js
@@ -14,7 +14,7 @@ export async function POST() {
                 subtitle: '요즘 마케터들은 콘텐츠 뭐 봄?',
                 description: '마케터들이 좋은 분위기에서 맛있는 음식을 먹으며 대화를 나누는 시간을 가질 예정입니다.',
                 status: 'closed',
-                createdAt: utcToZonedTime(new Date(), 'Asia/Seoul'),
+                createdAt: new Date(),
                 place: { name: '야옹식당', address: '서울특별시 강남구 청담동 123-4' },
                 placeStory: `WHAT WE WANT IS A PLACE
         WHERE EVERYONE CAN HANG OUT FREELY
@@ -37,77 +37,8 @@ export async function POST() {
                 participants: [
                     {
                         userId: '123',
-                        isLeader: true,
-                        payment: 'paid',
-                        attendCount: 0,
-                        createdAt: new Date(),
-                        cancelInfo: {
-                            isCancel: false,
-                            account: '',
-                            bank: '',
-                            reason: '',
-                            createdAt: null,
-                        },
-                    },
-                    {
-                        userId: '123',
                         isLeader: false,
-                        payment: 'unpaid',
-                        attendCount: 0,
-                        createdAt: new Date(),
-                        cancelInfo: {
-                            isCancel: false,
-                            account: '',
-                            bank: '',
-                            reason: '',
-                            createdAt: null,
-                        },
-                    },
-                ],
-            },
-            {
-                title: '콘텐츠 마케터 Sphere',
-                subtitle: '요즘 마케터들은 콘텐츠 뭐 봄?',
-                description: '마케터들이 좋은 분위기에서 맛있는 음식을 먹으며 대화를 나누는 시간을 가질 예정입니다.',
-                status: 'closed',
-                createdAt: utcToZonedTime(new Date(), 'Asia/Seoul'),
-                place: { name: '야옹식당', address: '서울특별시 강남구 청담동 123-4' },
-                placeStory: `WHAT WE WANT IS A PLACE
-        WHERE EVERYONE CAN HANG OUT FREELY
-        
-        아베크 청담은 18세기 프랑스 살롱 문화를 모티브로 하여
-        예술과 프라이빗한 공간이 어우러진 다이닝 카페, 바입니다.
-        
-        에피타이저부터 디저트까지 다채로운 디시 구성과
-        감각적인 인테리어의 공간에서 인사이트를 공유해보세요.`,
-                firstDate: new Date('2024-11-13T19:30:00'),
-                secondDate: new Date('2024-11-20T19:30:00'),
-                questions: [
-                    '당신의 최근 관심 콘텐츠는 무엇인가요?',
-                    '현재의 주요 업무와 도전 과제는 무엇인가요?',
-                    '앞으로의 계획은 무엇인가요?',
-                ],
-                thumbnail: '/sample-sphere.svg',
-                subImage1: '/sample-place-1.svg',
-                subImage2: '/sample-place-2.svg',
-                participants: [
-                    {
-                        userId: '123',
-                        isLeader: true,
-                        payment: 'paid',
-                        attendCount: 0,
-                        createdAt: new Date(),
-                        cancelInfo: {
-                            isCancel: false,
-                            account: '',
-                            bank: '',
-                            reason: '',
-                            createdAt: null,
-                        },
-                    },
-                    {
-                        userId: '123',
-                        isLeader: false,
+                        requestLeader: true,
                         payment: 'unpaid',
                         attendCount: 0,
                         createdAt: new Date(),
