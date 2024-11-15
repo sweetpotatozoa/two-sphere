4c820962c646db5b5b58b58a25f516a304623397
01tilinfinity <kuwinter0209@gmail.com>
2024-11-15 21:38:58 +0900

    fix:secondDate 표시

--- components/SphereHeader.jsx
+++ components/SphereHeader.jsx
@@ -4,7 +4,7 @@ import locationIcon from '/public/location-icon-black.svg';
 import calendarIcon from '/public/calendar-icon-black.svg';
 
 // wrong child 문제로 sphere 하위 객체 불러오는 코드 수정
-const SphereHeader = ({ title, subtitle, place, firstDate }) => (
+const SphereHeader = ({ title, subtitle, place, firstDate, secondDate }) => (
     <section className="border-b border-black pb-8">
         <h1 className="text-2xl font-extrabold mb-1">{title}</h1>
         <p className="mb-4 font-bold">{subtitle}</p>
@@ -15,7 +15,9 @@ const SphereHeader = ({ title, subtitle, place, firstDate }) => (
             </div>
             <div className="flex items-center justify-center space-x-2">
                 <Image src={calendarIcon} alt="Calendar Icon" width={16} height={16} />
-                <span>{firstDate}</span>
+                <span>
+                    {firstDate} / {secondDate}
+                </span>
             </div>
         </div>
     </section>
